a75b2e19b175295943a42ea59d4bfd5a8284006a
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-09-25 23:45:01 +0500

    This is not updating state, when we click on button but in console we see icrement is done/performed

---
+++ lib/02._counter_view_i.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CounterViewTwo extends StatefulWidget {
+  const CounterViewTwo({super.key});
+
+  @override
+  State<CounterViewTwo> createState() => _CounterViewTwoState();
+}
+//This is not updating on State if we increment value,
+// yes, when we see debug console there we see increment but on screen
+// only increment a counter value but not updating state.
+
+// Counter App  Using ElrvatedButton > OnPressed(){}
+class _CounterViewTwoState extends State<CounterViewTwo> {
+  int counter = 5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text(
+              "Counter Value: $counter",
+              style: TextStyle(fontSize: 40),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                counter++;
+                print(counter);
+              },
+              child: Text("Add Value"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
